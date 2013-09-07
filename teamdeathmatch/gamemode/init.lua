6ab21358a6daff902d50f05e299115b9da53e189
mightmadtv <mightymadtv@gobcraft.net>
2013-09-07 17:51:42 +0200

    Added give ammo

--- teamdeathmatch/gamemode/init.lua
+++ teamdeathmatch/gamemode/init.lua
@@ -16,6 +16,7 @@ function blue( ply )
     ply:StripWeapons()
     ply:Give( "weapon_crossbow" )
     ply:Give( "weapon_crowbar" )
+    ply.GiveAmmo(50, "XBowBolt")
  
 end
 concommand.Add( "blue", blue )
@@ -27,6 +28,7 @@ function orange( ply )
     ply:StripWeapons()
     ply:Give( "weapon_pistol" )
     ply:Give( "weapon_stunstick" )
+    ply.GiveAmmo(100, "Pistol")
 end
 concommand.Add( "orange", orange )
 
