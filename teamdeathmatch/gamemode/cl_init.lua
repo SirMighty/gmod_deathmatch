cdbc331ad3a6b49fa54286c37db8da16f373ae9a
mightmadtv <mightymadtv@gobcraft.net>
2013-09-20 17:18:10 +0200

    Fixed Player name Position

--- teamdeathmatch/gamemode/cl_init.lua
+++ teamdeathmatch/gamemode/cl_init.lua
@@ -20,9 +20,9 @@ function DrawPlayerInfo( )
 			if( v:Alive( ) ) then
 			
 				local alpha = 0
-				local color = team.GetColor(v:Team())
+				local color = Color(255,255,255,alpha)
 				local position = v:GetPos( )
-				local position = Vector( position.x + 50, position.y, position.z + 75 )
+				local position = Vector( position.x, position.y, position.z + 75 )
 				local screenpos = position:ToScreen( )
 				local dist = position:Distance( LocalPlayer( ):GetPos( ) )
 				local dist = dist / 4
@@ -50,7 +50,7 @@ function DrawPlayerInfo( )
 				if( v:GetNWString("vTitle") == nil ) then
 					draw.SimpleTextOutlined(v:Nick(), "Trebuchet24", screenpos.x, screenpos.y, Color(team.GetColor(v:Team()).r, team.GetColor(v:Team()).g, team.GetColor(v:Team()).b, alpha), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, 2, Color(0,0,0,alpha))
 				else
-					draw.SimpleText(v:Nick(), "Trebuchet24", screenpos.x, screenpos.y, Color(color.r, color.g,color.b,alpha), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, 2, Color(0,0,0,alpha))
+					draw.SimpleTextOutlined(v:Nick(), "Trebuchet24", screenpos.x, screenpos.y, color, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, 2, Color(0,0,0,alpha))
 					--draw.SimpleText(v:GetNWString("vTitle"), "Trebuchet24", screenpos.x, screenpos.y+27, Color(255,255,255,alpha), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, 2, Color(0,0,0,alpha))				
 				end
 				if( v:GetNWInt("chatopen") == 1 ) then
